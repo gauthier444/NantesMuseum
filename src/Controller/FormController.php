f42f3f33af2da37c75d46461ce9761580e04ab61
Jeremie B <jeremie351@gmail.com>
2019-02-19 11:39:20 +0100

    formulaire

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,20 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Form;
+use App\Form\FormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FormController extends AbstractController
 {
     /**
      * @Route("/form", name="form")
      */
-    public function index()
+    public function index(Request $request, EntityManagerInterface $manager)
     {
-        $form = $this->createForm(Form::class, new Form());
+        $form = $this->createForm(FormType::class, new Form());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($form->getData());
+            $manager->flush();
+            $form->getData();
+            return $this->redirectToRoute('/form');
+        }
 
         return $this->render('form/index.html.twig', [
             'form' => $form->createView(),
         ]);
+
+
     }
 }
