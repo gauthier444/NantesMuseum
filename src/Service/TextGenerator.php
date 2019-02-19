a7a50c270268aed4bbe5c8667a7bbf78dba00dd5
Jeremie B <jeremie351@gmail.com>
2019-02-19 16:13:38 +0100

    traduction fr

---
+++ src/Service/TextGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class TextGenerator
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+}
+
