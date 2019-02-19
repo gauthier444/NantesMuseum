d61e2dd88e63b7a9af34ae1bc710b451f0732541
Jeremie B <jeremie351@gmail.com>
2019-02-19 15:14:16 +0100

    modif réservation

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +17,21 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name')
-            ->add('FirstName')
-            ->add('PhoneNumber')
-            ->add('Mail')
-            ->add('MuseumChoice')
-            ->add('Date')
+            ->add('Name', TextType::class)
+            ->add('FirstName', TextType::class)
+            ->add('PhoneNumber', NumberType::class)
+            ->add('Mail', EmailType::class)
+            ->add('Date', DateTimeType::class)
+            ->add('MuseumChoice', ChoiceType::class, [
+                'choices' => [
+                    'Musée d\'histoire' => 'museum1',
+                    'Musée d\'arts de Nantes' => 'museum2',
+                    'Musée Dobrée' => 'museum3',
+                    'Musée Jules Verne' => 'museum4',
+                    'Musée d\'histoire naturelle' => 'museum5',
+                ],
+            ])
+            ->add('Date', DateTimeType::class)
         ;
     }
 
